c7622c9435b6de0090c89d68adc18f016ef01034
Adam Derylo <aderylo@noreply>
2024-09-29 20:36:32 +0200

    Initial commit

---
+++ test/aws-guardrails.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsGuardrails from '../lib/aws-guardrails-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-guardrails-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsGuardrails.AwsGuardrailsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
